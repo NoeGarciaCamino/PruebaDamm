ffcb24f31c55e6d64ff31f69e12d178949bc83d3
odomenec <odomenec@everis.com>
2022-07-19 13:54:10 +0200

    Refactorizado nombre de los runner

--- everis-app-mobile-appium-java-master/src/test/java/com/everis/ct/mobile/runner/RunneriPhone.java
+++ everis-app-mobile-appium-java-master/src/test/java/com/everis/ct/mobile/runner/RunneriPhone.java
@@ -1,22 +1,21 @@
 package com.everis.ct.mobile.runner;
 
-import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.FeatureWrapper;
-import io.cucumber.testng.PickleWrapper;
-import io.cucumber.testng.TestNGCucumberRunner;
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
+import io.cucumber.testng.*;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 @CucumberOptions(
 
 //        publish = false,
 //        stepNotifications = true,
-
 //      ******   Para lanzar una feature concreta   ******
         features = {"src/test/resources/features/1.login.feature"},
 //      ******   Para lanzar todas las features   ******
@@ -26,20 +25,23 @@ import java.util.logging.Logger;
 
         plugin = {"html:target/cucumber-report.html", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
-public class RunnerGooglePixel {
-
+public class RunneriPhone {
     private TestNGCucumberRunner testNGCucumberRunner;
 
     @BeforeClass
     public static void beforeExecution() {
-        Logger.getLogger(RunnerGooglePixel.class.getName()).log(Level.INFO, "BEFORE EXECUTION --->");
+        Logger.getLogger(RunneriPhone.class.getName()).log(Level.INFO, "BEFORE EXECUTION --->");
     }
 
     @AfterClass
     public static void afterExecution() {
-        Logger.getLogger(RunnerGooglePixel.class.getName()).log(Level.INFO, "AFTER EXECUTION --->");
+        Logger.getLogger(RunneriPhone.class.getName()).log(Level.INFO, "AFTER EXECUTION --->");
     }
+    @Listeners({ExtentITestListenerClassAdapter.class})
+    public class RunCucumberTest extends AbstractTestNGCucumberTests
+    {
 
+    }
     @Test(groups = "cucumber", description = "Run Cucumber Features.", dataProvider = "scenarios", invocationCount=1)
     public void scenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
         testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
